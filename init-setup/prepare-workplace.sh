18b2cd5e57518ccb7acb0f57ee9c40c5a9bd37a1
Luis Acosta <luis13cst@gmail.com>
2021-12-11 14:59:35 +0100

    feat: add script for setup new workplace PC

---
+++ init-setup/prepare-workplace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y nmap net-tools yakuake colordiff htop docker.io mysql-client filezilla python3 gnupg software-properties-common curl terminator gparted git rkhunter virtualbox vagrant
+sudo snap install go --classic
+mkdir ~/Bucket/ ~/Docker/ ~/Scripts/ ~/TestArea/
+sudo mkdir -p /opt/git/luis13byte /opt/tools /opt/vagrant/ /backup/_special
+sudo chown ${USER}: /opt/git
+
+# Git config
+cat << EOF > /opt/git/.gitignore_global
+# IDEs
+.idea/
+.vscode/
+.history/
+EOF
+
+sudo chown ${USER}: /opt/git/ /opt/vagrant/ /backup/ -R
+git config --global core.excludesfile /opt/git/.gitignore_global
+
+# Kubernetes
+wget https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz
+tar xf kubeval-linux-amd64.tar.gz
+sudo cp kubeval /usr/local/bin
