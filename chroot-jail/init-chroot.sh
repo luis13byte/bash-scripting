4ea5617f3b6bf9342797302cbbc98a3b61c162fe
Luis Acosta <luis13cst@gmail.com>
2021-12-14 11:47:37 +0100

    feat: add editable directory for chroot user

--- chroot-jail/init-chroot.sh
+++ chroot-jail/init-chroot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Initializate chroot jail
-# first must create user and configure sshd_config for determine $USERHOME like home
+# first must create user and configure sshd_config for determine $USERHOME like ChrootDirectory
 
 USER="developer"
-USERHOME=""
+USERHOME="/home/developer"
 
 [[ $(id "${USER}" &>/dev/null) -eq 0 ]] || useradd ${USER}
 cd ${USERHOME}
@@ -15,11 +15,14 @@ mknod -m 666 zero c 1 5
 mknod -m 666 random c 1 8
 
 chown root:root ${USERHOME} && chmod 755 ${USERHOME}
-mkdir ${USERHOME}/bin && cp /bin/bash ${USERHOME}/bin/
 
+mkdir ${USERHOME}/etc/ ${USERHOME}/home/
+cp /etc/{passwd,group} ${USERHOME}/etc/
+chown ${USER}: ${USERHOME}/home/
+
+# Enable bash use
+mkdir ${USERHOME}/bin && cp /bin/bash ${USERHOME}/bin/
 mkdir -p ${USERHOME}/lib/x86_64-linux-gnu ${USERHOME}/lib64/
 cp /lib/x86_64-linux-gnu/{libtinfo.so.6,libdl.so.2,libc.so.6} ${USERHOME}/lib/x86_64-linux-gnu/
 cp /lib64/ld-linux-x86-64.so.2 ${USERHOME}/lib64/
 
-mkdir ${USERHOME}/etc
-cp /etc/{passwd,group} ${USERHOME}/etc/
