5cc6a976ea35cc7b90cd19ae8f57ffa431e16e66
Luis Acosta <luis13cst@gmail.com>
2021-12-22 12:11:17 +0100

    feat: add docker-compose installation

--- init-setup/prepare-workplace.sh
+++ init-setup/prepare-workplace.sh
@@ -17,7 +17,12 @@ EOF
 sudo chown ${USER}: /opt/git/ /opt/vagrant/ /backup/ -R
 git config --global core.excludesfile /opt/git/.gitignore_global
 
-# Kubernetes
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Kubernetes tool
 wget https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz
 tar xf kubeval-linux-amd64.tar.gz
 sudo cp kubeval /usr/local/bin
