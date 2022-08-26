7ac94c5e7d936c39b4797bca5b93e28bd65e2f66
Luis Acosta <luis13cst@gmail.com>
2022-08-26 16:41:58 +0200

    feat: add kubectl tool & structure improvements

--- init-setup/prepare-workplace.sh
+++ init-setup/prepare-workplace.sh
@@ -1,48 +1,73 @@
 #!/bin/bash
+set -e # enable errexit option
+set -u # enable nounset option
+set -x # enable xtrace option
 
-sudo apt update && sudo apt install -y nmap tree net-tools tmux yakuake colordiff htop docker.io dnsutils mysql-client postgresql-client filezilla python3 gnupg software-properties-common curl jq gparted zsh git-core git rkhunter virtualbox locate vagrant
-sudo snap install go --classic && echo "export PATH=$HOME/bin:/usr/local/bin:$HOME/go/bin:$PATH" >> ~/.zshrc
-mkdir ~/Bucket/ ~/Docker/ ~/Scripts/ ~/TestArea/
-sudo mkdir -p /opt/git/luis13byte /opt/tools /opt/vagrant/ /backup/_special
-sudo chown ${USER}: /opt/git
+readonly GIT_USER="luis13byte"
 
-# Git config
-cat << EOF > /opt/git/.gitignore_global
-# IDEs
-.idea/
-.vscode/
-.history/
-EOF
+installPackages() {
+  sudo apt update && sudo apt install -y nmap tree net-tools tmux clipit colordiff htop docker.io dnsutils mysql-client postgresql-client filezilla python3 \
+    gnupg software-properties-common curl jq gparted zsh git-core git rkhunter virtualbox locate vagrant
+  sudo snap install go --classic && echo "export PATH=$HOME/bin:/usr/local/bin:$HOME/go/bin:$PATH" >> ~/.zshrc
+  mkdir ~/Bucket/ ~/Kubernetes/ ~/Scripts/ ~/TestArea/
+  sudo mkdir -p /opt/git/${GIT_USER} /opt/tools /opt/vagrant/ /backup/_special
+  sudo chown ${USER}: /opt/git
+}
 
-sudo chown ${USER}: /opt/git/ /opt/vagrant/ /backup/ -R
-git config --global core.excludesfile /opt/git/.gitignore_global
+gitConfig() {
+  cat << EOF > /opt/git/.gitignore_global
+  # IDEs
+  .idea/
+  .vscode/
+  .history/
+  EOF
 
-# Install Alacritty
-sudo add-apt-repository ppa:mmstick76/alacritty && sudo apt install alacritty -y \
-&& mkdir -p $HOME/.config/alacritty/
+  sudo chown ${USER}: /opt/git/ /opt/vagrant/ /backup/ -R
+  git config --global core.excludesfile /opt/git/.gitignore_global
+}
+
+installAlacritty() {
+  sudo add-apt-repository ppa:mmstick76/alacritty && sudo apt install alacritty -y \
+    && mkdir -p $HOME/.config/alacritty/
+}
 
 # Custom tmux (Dracula Theme)
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm \
-&& wget https://raw.githubusercontent.com/luis13byte/dotfiles/master/tmux/.tmux.conf -O ~/.tmux.conf \
-&& echo "Necessary reload tmux environment. Press <prefix> + I (capital i) in tmux windows to install plugins."
+tmuxConfig() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm \
+   && wget https://raw.githubusercontent.com/luis13byte/dotfiles/master/tmux/.tmux.conf -O ~/.tmux.conf \
+   && echo "Necessary reload tmux environment. Press <prefix> + I (capital i) in tmux windows to install plugins."
+}
 
-# Config docker and install docker-compose
-sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-&& sudo chmod +x /usr/local/bin/docker-compose \
-&& sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+dockerConfig() {
+  sudo usermod -aG docker $USER
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+    && sudo chmod +x /usr/local/bin/docker-compose \
+    && sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
 
-# Install Kubeval tool
-wget https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz
-tar xf kubeval-linux-amd64.tar.gz
-sudo cp kubeval /usr/local/bin
+installKubectl() {
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && mkdir /${USER}/.kube
+}
 
-# Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/install-chrome.deb \
-&& sudo apt install /tmp/install-chrome.deb
+installChrome() {
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/install-chrome.deb \
+    && sudo apt install /tmp/install-chrome.deb
+}
 
-# Install Podman
-source /etc/os-release
-sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
-sudo apt update -qq && sudo apt-get -qq --yes install podman
+installPodman() {
+  source /etc/os-release
+  sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
+  sudo apt update -qq && sudo apt-get -qq --yes install podman
+}
+
+# Run functions
+installPackages
+gitConfig
+installAlacritty
+tmuxConfig
+dockerConfig
+installKubectl
+installChrome
+installPodman
