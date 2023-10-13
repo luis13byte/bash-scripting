fb5f23eb0c3375faedac973e229ab541b5af22fc
Luis Acosta <luis13cst@gmail.com>
2023-10-13 16:28:39 +0200

    Update prepare-workplace.sh

--- init-setup/prepare-workplace.sh
+++ init-setup/prepare-workplace.sh
@@ -6,23 +6,29 @@ set -x # enable xtrace option
 readonly GIT_USER="luis13byte"
 
 installPackages() {
-  sudo apt update && sudo apt install -y nmap tree net-tools tmux clipit colordiff htop docker.io dnsutils mysql-client postgresql-client filezilla python3 \
-    gnupg software-properties-common curl jq gparted zsh git-core git rkhunter virtualbox locate vagrant
-  sudo snap install go --classic && echo "export PATH=$HOME/bin:/usr/local/bin:$HOME/go/bin:$PATH" >> ~/.zshrc
-  mkdir ~/Bucket/ ~/Kubernetes/ ~/Scripts/ ~/TestArea/
-  sudo mkdir -p /opt/git/${GIT_USER} /opt/tools /opt/vagrant/ /backup/_special
+  sudo add-apt-repository ppa:shutter/ppa
+  sudo apt-get update && sudo apt-get install -y nmap tree vlc net-tools tmux clipit colordiff htop docker.io dnsutils mysql-client postgresql-client filezilla python3 \
+    gnupg software-properties-common curl jq gparted zsh git-core git rkhunter locate shutter
+
+  # Snap Installs
+  sudo snap install go --classic && 
+  sudo snap install code --classic
+
+  # Ending
+  echo "export PATH=$HOME/bin:/usr/local/bin:$HOME/go/bin:$PATH" >> ~/.zshrc
+  sudo mkdir -p /opt/git/${GIT_USER} /opt/tools /backup/_special ~/Bucket/ ~/Scripts/ ~/TestArea/
   sudo chown ${USER}: /opt/git
 }
 
 gitConfig() {
-  cat << EOF > /opt/git/.gitignore_global
-  # IDEs
-  .idea/
-  .vscode/
-  .history/
+cat << 'EOF' > /opt/git/.gitignore_global
+# Exclusiones para IDEs y herramientas
+.idea/
+.vscode/
+.history/
 EOF
 
-  sudo chown ${USER}: /opt/git/ /opt/vagrant/ /backup/ -R
+  sudo chown ${USER}: /opt/git/ /backup/ -R
   git config --global core.excludesfile /opt/git/.gitignore_global
 }
 
@@ -40,7 +46,7 @@ tmuxConfig() {
 
 dockerConfig() {
   sudo usermod -aG docker $USER
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+  sudo curl -L "https://github.com/docker/compose/releases/download/2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
     && sudo chmod +x /usr/local/bin/docker-compose \
     && sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
@@ -52,14 +58,7 @@ installKubectl() {
 
 installChrome() {
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/install-chrome.deb \
-    && sudo apt install /tmp/install-chrome.deb
-}
-
-installPodman() {
-  source /etc/os-release
-  sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
-  sudo apt update -qq && sudo apt-get -qq --yes install podman
+    && sudo apt-get install /tmp/install-chrome.deb
 }
 
 # Run functions
@@ -70,4 +69,3 @@ tmuxConfig
 dockerConfig
 installKubectl
 installChrome
-installPodman
